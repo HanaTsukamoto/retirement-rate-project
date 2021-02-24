6adbe02b85f2e21d0508620c32e6939b7af7ce03
Hana Tsukamoto <fujiyamahannah@gmail.com>
2021-02-24 11:23:12 +0900

    プログラムを保存します

--- userinterface/retirement_controller.py
+++ userinterface/retirement_controller.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 hr_df = pd.read_csv("./社員データ.csv")
 社員データ = hr_df[['satisfaction_level', 'last_evaluation', 'number_project']]
 社員ID = hr_df['社員ID']
+parent_directory = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/predict', methods=["POST"])
 def predict():
@@ -15,13 +16,12 @@ def predict():
     社員の退職率を予測します。
     社員のデータを受け取って、退職率予測モデルに入力して、退職率を0~100の間で返します。
     """
-    url = 'http://localhost:8080/predict'
-    satisfaction_level =  request.json.post(url, 'satisfaction_level')
-    last_evaluation =  request.json.post(url, 'last_evaluation')
-    number_project =  request.json.post(url, 'number_project')
-
-    loaded_model = pickle.load(open("/Users/hana/ml/retirement-rate-project/退職率計算モデル.sav", "rb"))
-    #  相対パス
+    url = "http://localhost:8080/predict"
+    satisfaction_level =  request.json(url, "satisfaction_level")
+    last_evaluation =  request.json(url, "last_evaluation")
+    number_project =  request.json(url, "number_project")
+    # loaded_model = pickle.load(open("/Users/hana/ml/retirement-rate-project/退職率計算モデル.sav", "rb"))
+    loaded_model = pickle.load(open(parent_directory + "/../退職率計算モデル", 'rb'))
     x = [satisfaction_level, last_evaluation, number_project]
 
     return jsonify({
@@ -37,7 +37,8 @@ def all():
     退職率の高い社員一覧を表示する
     社員の退職率データを受け取って退職率が75%以上の社員を表示
     """ 
-    loaded_model = pickle.load(open("/Users/hana/ml/retirement-rate-project/退職率計算モデル.sav", "rb"))
+    # loaded_model = pickle.load(open("/Users/hana/ml/retirement-rate-project/退職率計算モデル.sav", "rb"))
+    loaded_model = pickle.load(open(parent_directory + "/../退職率計算モデル", 'rb'))
     退職率 = loaded_model.predict_proba(社員データ)[:, 1]
     # print(退職率)
     # loadedmodel print　method の戻り値
