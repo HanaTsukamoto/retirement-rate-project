053aafe3f35a5d3b75802dab20acfd18a6478ef9
Hana Tsukamoto <fujiyamahannah@gmail.com>
2021-02-26 11:22:30 +0900

    プログラムを保存します

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ cd /path/to/retirement-rate-project
 
 git add .
 git commit -m "プログラムを保存します"
-git push origin main
+git push origin master
 ```
 
 ```bash
@@ -15,9 +15,9 @@ python userinterface/retirement_controller.py
 # 社員の退職率を予測する
 curl -XPOST "http://localhost:8080/predict" -H "Content-type:application/json" -d '
 {
-    satisfaction_level = 9.2,
-    last_evaluation = 2.2,
-    number_project = 6.8 
+    "satisfaction_level": 5.0,
+    "last_evaluation": 3.2,
+    "number_project": 10
 }
 '
 
--- userinterface/retirement_controller.py
+++ userinterface/retirement_controller.py
@@ -16,17 +16,17 @@ def predict():
     社員の退職率を予測します。
     社員のデータを受け取って、退職率予測モデルに入力して、退職率を0~100の間で返します。
     """
-    url = "http://localhost:8080/predict"
-    satisfaction_level =  request.json(url, "satisfaction_level")
-    last_evaluation =  request.json(url, "last_evaluation")
-    number_project =  request.json(url, "number_project")
-    # loaded_model = pickle.load(open("/Users/hana/ml/retirement-rate-project/退職率計算モデル.sav", "rb"))
-    loaded_model = pickle.load(open(parent_directory + "/../退職率計算モデル", 'rb'))
-    x = [satisfaction_level, last_evaluation, number_project]
 
+    # request.data
+    satisfaction_level = request.json["satisfaction_level"]
+    # print(satisfaction_level)
+    last_evaluation = request.json["last_evaluation"]
+    number_project = request.json["number_project"]
+    loaded_model = pickle.load(open(parent_directory + "/../退職率計算モデル.sav", 'rb'))
+    x = [satisfaction_level, last_evaluation, number_project]
+    # exit()
     return jsonify({
         "retirement rate": str(loaded_model.predict([x])[0])
-        # "retirement rate": str(loaded_model.predict(社員データ)[0])
         #  辞書型に格納されているうち0番目の値をstringとして返す
     })
 
@@ -37,22 +37,19 @@ def all():
     退職率の高い社員一覧を表示する
     社員の退職率データを受け取って退職率が75%以上の社員を表示
     """ 
-    # loaded_model = pickle.load(open("/Users/hana/ml/retirement-rate-project/退職率計算モデル.sav", "rb"))
-    loaded_model = pickle.load(open(parent_directory + "/../退職率計算モデル", 'rb'))
+    loaded_model = pickle.load(open(parent_directory + "/../退職率計算モデル.sav", 'rb'))
     退職率 = loaded_model.predict_proba(社員データ)[:, 1]
     # print(退職率)
     # loadedmodel print　method の戻り値
-    # print(loaded_model.predict_proba(社員データ)[:, 1])
     
     退職率一覧 = {}
     for i in range(len(社員データ)):
        if 退職率[i] >= 0.75:
-           退職率一覧[社員ID[i]] = 退職率[i]
+           退職率一覧[int(社員ID[i])] = 退職率[i]
                    
     return jsonify({
         "退職率が高い社員一覧": 退職率一覧
     })
-
 
 
 if __name__ == "__main__":
